ec6af783ffe9c343f20af46ccd97c3f1f4103649
sandeep654 <73191150+sandeep654@users.noreply.github.com>
2023-12-24 09:01:51 +0530

    Aysnc-await

---
+++ notes.js
@@ -0,0 +1,18 @@
+// javaScript is synchronous and single threaded
+
+// javaScript is prototype based language 
+
+// javascript execute the code one by one 
+
+// blocking code vs non blocking code
+
+// 1. blocking code are
+
+// block the flow of program 
+// -Read  file sync
+
+// 2. Non - blocking code are
+
+// -don't block code execution 
+// -Read file Async
+
